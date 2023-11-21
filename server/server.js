22fe78b9d1906c8b406d150624ca7c6c18a9d164
Pavel Ivanov <pavel.2000.vd@gmail.com>
2023-11-21 12:28:42 +0200

    server fix

--- server/server.js
+++ server/server.js
@@ -10,45 +10,35 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const todoSchema = new mongoose.Schema({
-  day: { type: Date},
-  todos: { type: [String] },
+  day: { type: Date },
+  todos: { type: [String] }
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
 
 
 mongoose.connect('mongodb://0.0.0.0:27017/to-do-app', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log('MongoDB database connection established successfully');
+  console.log('MongoDB database connection established successfully');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
 
 app.post('/todos/save', async (req, res) => {
-  const { todos } = req.body;
+  const { day, todos } = req.body;
 
   try {
 
-    if (Array.isArray(todos)) {
- 
-      const day = todos.day; 
+    const savedTodo = await Todo.create({ day: day, todos: todos });
 
-      // const tasks = todos.map(todo => todo.todos);
-      const tasks = "dddd"
-
-      const savedTodo = await Todo.create({ day: day, todos: tasks });
-
-      res.json(savedTodo);
-    }  else {
-      throw new Error('Invalid data format');
-    }
+    res.json(savedTodo);
   } catch (error) {
     console.error('Error saving to-do items:', error);
     res.status(500).json({ error: 'Error saving to-do items' });
